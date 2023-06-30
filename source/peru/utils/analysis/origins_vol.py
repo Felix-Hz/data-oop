5d86796674c88a1afe41ddc916664074a16d3ac8
Felix-Hz <feluhv@outlook.es>
2023-06-30 13:00:15 +0200

    modif: df creation in origins_vol & top_imports

--- source/peru/utils/analysis/origins_vol.py
+++ source/peru/utils/analysis/origins_vol.py
@@ -1,24 +1,10 @@
 import pandas as pd
 
-
 def analyzing_origins(dfs):
-    '''
-
-        Analyses the origin of imported goods over multiple years for given datasets.
-
-            Parameters:
-                - dfs (list): A list of pandas DataFrames containing data on imported goods.
-
-            Returns:
-                - DataFrame: Information on the origins of imported goods over multiple years.
-
-    '''
-
     excel_new_data = pd.DataFrame()
 
     for df in dfs:
-
-        volumenTotalImportacionTn = (df['Kgs. Netos'].sum()/1000).round(2)
+        volumenTotalImportacionTn = (df['Kgs. Netos'].sum() / 1000).round(2)
 
         registro_volumen = {
             "NCM": [],
@@ -30,31 +16,23 @@ def analyzing_origins(dfs):
         }
 
         for pais in df['País de Origen'].unique():
+            data = df[df['País de Origen'] == pais]
+            volumenTotalOrigen = (data['Kgs. Netos'].sum() / 1000).round(2)
 
-            data = df[df['País de Origen'] == f"{pais}"]
-
-            volumenTotalOrigen = (data['Kgs. Netos'].sum()/1000).round(2)
-
-            if (data['Kgs. Netos'].sum() > 100000):
-
+            if data['Kgs. Netos'].sum() > 100000:
                 registro_volumen['NCM'].append(''.join(df['Código NCM'].unique().astype(str).tolist()))
-                registro_volumen['Año'].append(
-                    str(data["Fecha"].iloc[0].year)[:4])
+                registro_volumen['Año'].append(str(data["Fecha"].iloc[0].year)[:4])
                 registro_volumen['Pais'].append(pais)
                 registro_volumen['No. Importaciones'].append(len(data))
-                registro_volumen['Volumen Total (TN)'].append(
-                    (data['Kgs. Netos'].sum()/1000).round(2))
-                registro_volumen['Participacion en Vol. Total'].append(
-                    f"{round(( volumenTotalOrigen / volumenTotalImportacionTn) *100)}%")
-                print(f"- Done with: {pais} in {df['Código NCM'].iloc[0]} ({df['Fecha'].iloc[0].year}) ")
+                registro_volumen['Volumen Total (TN)'].append(volumenTotalOrigen)
+                registro_volumen['Participacion en Vol. Total'].append(f"{round((volumenTotalOrigen / volumenTotalImportacionTn) * 100)}%")
+                print(f"- Done with: {pais} in {df['Código NCM'].iloc[0]} ({df['Fecha'].iloc[0].year})")
 
-        transition_df = pd.DataFrame.from_records(registro_volumen).sort_values(
-            'Volumen Total (TN)', ascending=False).reset_index(drop=True)
-        transition_df = transition_df[[
-            'NCM', 'Año', 'Pais', 'Volumen Total (TN)', "Participacion en Vol. Total", 'No. Importaciones']]
-        excel_new_data = excel_new_data.append(
-            transition_df, ignore_index=False)
+        transition_df = pd.DataFrame.from_records(registro_volumen).sort_values('Volumen Total (TN)', ascending=False).reset_index(drop=True)
+        transition_df = transition_df[['NCM', 'Año', 'Pais', 'Volumen Total (TN)', "Participacion en Vol. Total", 'No. Importaciones']]
+        excel_new_data = pd.concat([excel_new_data, transition_df], ignore_index=True)
 
     print("~~~~~~~~~~~~~~~~~~~\n> Current dataframe of the origins of each year:")
+    print(excel_new_data)
 
     return excel_new_data
